1c61d0d60d3577b808b285dff1541f2bd6e4e81c
Sudhanshu Jha <Sudhanshujha2717@gmail.com>
2023-08-10 22:52:20 +0530

    day1

---
+++ basic1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    char ch[6];
+
+    //input
+    cin>>ch;  //12345  last wala \0 hota hai
+
+    //output
+    cout<<ch;  //12345s
+}
